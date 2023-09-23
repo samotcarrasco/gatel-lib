073423994779f9073bbded1f30a2c2acea7ad61c
samotcarrasco <samotcarrasco@gmail.com>
2023-09-23 23:46:55 +0200

    interface TipoEquipoInformatico

--- lib/src/main/java/es/mde/acing/gatel/TipoEquipoInformatico.java
+++ lib/src/main/java/es/mde/acing/gatel/TipoEquipoInformatico.java
@@ -2,35 +2,10 @@ package es.mde.acing.gatel;
 
 import java.util.List;
 
-public class TipoEquipoInformatico {
-	
-	private Long id;
-	private String nombre;
-	private List<EquipoInformatico> equipos;
+public interface TipoEquipoInformatico {
 
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public List<EquipoInformatico> getEquipos() {
-		return equipos;
-	}
-	public void setEquipos(List<EquipoInformatico> equipos) {
-		this.equipos = equipos;
-	}
-	@Override
-	public String toString() {
-		return "TipoEquipoInformatico [id=" + id + ", nombre=" + nombre + "]";
-	}
-	
+	String getNombre();
+
+	List<EquipoInformatico> getEquipos();
+
 }
